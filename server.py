b8040ed0e89167580825ab8c4a168a6ff27364f5
Ahmed Alhakimi <alhakimia542@gmail.com>
2025-11-25 23:39:59 +0300

    Add HTML interface and update server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import tensorflow as tf
 from PIL import Image
 import numpy as np
@@ -6,30 +6,30 @@ import os
 
 app = Flask(__name__)
 
-MODEL_PATH = "final_skin_model.keras"  # تأكد أن الملف موجود في نفس المجلد
-
+# -----------------------------
+MODEL_PATH = "final_skin_model.keras"
+# -----------------------------
 if not os.path.exists(MODEL_PATH):
     raise FileNotFoundError(f"Model file not found: {MODEL_PATH}")
 
 model = tf.keras.models.load_model(MODEL_PATH)
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 @app.route("/predict", methods=["POST"])
 def predict():
     if "file" not in request.files:
         return jsonify({"error": "No file uploaded"}), 400
-
     file = request.files["file"]
-
     try:
         img = Image.open(file).resize((128, 128))
         img = np.array(img)/255.0
         img = img.reshape(1, 128, 128, 3)
-
         pred = model.predict(img)[0][0]
-        label = "خلايا سلاطانية" if pred >= 0.5 else "طبيعي"
-
+        label = "خلايا سرطانية" if pred >= 0.5 else "طبيعي"
         return jsonify({"label": label, "score": float(pred)})
-
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
